798546fb5369cbcebc7fe22f83a65f86064e79e7
vidhihihihihi <gupta.74@iitj.ac.in>
2023-08-06 01:02:44 +0530

    new changes

--- netflix-clone/src/Banner.js
+++ netflix-clone/src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from './axios';
 import requests from './requests';
+import './Banner.css';
 
 function Banner(){
     const [movie, setMovie] = useState([]);
@@ -15,8 +16,12 @@ function Banner(){
     fetchData();
     }, []);
 
-    console.log(movie,"~movie");
-
+    console.log(movie);
+    
+    function truncate(str,n) {
+        return str?.length > n ? str.substr(0,n-1) + "..." : str;
+    
+    }
     return(
         <header className='banner'
         style={{
@@ -25,9 +30,9 @@ function Banner(){
             backdropPosition: "center center",
         }}
         >
-        <div className='banner_contets'>
+        <div className='banner_contents'>
        
-        <h1>
+        <h1 className='banner_title'>
         {movie?.title || movie?.name || movie?.original_name} 
         </h1>
          <div className='banner_buttons'>
@@ -35,8 +40,9 @@ function Banner(){
           <button className='banner_button'>My List</button>
 
          </div>
-         <h1 className='banner_description'>{movie?.overview}</h1>
+         <h1 className='banner_description'>{truncate(movie?.overview,150)}</h1>
         </div>
+        <div className='banner--fadeBottom'/>
         </header>
     );
 }
--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,37 +1,60 @@
-import React , { useState, useEffect } from "react";
-import instance from "./axios";
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
 import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const baseurl = "http://image.tmdb.org/t/p/original/";
 
-function Row({title, fetchUrl}) {
-    const [movies, setMovies] = useState([]) ;
-    useEffect(() => {
-     async function fetchData() {
-        const request = await instance.get(fetchUrl); 
-        setMovies(request.data.results);
-        return request;
-     }
-     fetchData();
-    },[fetchUrl]);
-
-    
-    return(
-        <div className='row'>
-            <h2>{title}</h2>
-            <div className='row_posters'>
-             {movies.map((movie)=>(
-                <img 
-                 key={movie.id}
-                 className="row_poster"
-                 src={`${baseurl}${movie.poster_path}`} 
-                 alt={movie.name}/>
-             ))}
-            </div>
-           
-
-        </div>
-    );
+function Row({ title, fetchUrl, isLargeRow }) {
+	const [movies, setMovies] = useState([]);
+	const [trailerUrl, setTrailerUrl] = useState("");
+	useEffect(() => {
+		async function fetchData() {
+			const request = await axios.get(fetchUrl);
+			setMovies(request.data.results);
+			return request;
+		}
+		fetchData();
+	}, [fetchUrl]);
+	const opts = {
+		height: "390",
+		width: "100%",
+		playerVars: {
+			autoplay: 1,
+		},
+	};
+	function handleClick(movie) {
+		console.log(movie);
+		if (trailerUrl) {
+			setTrailerUrl("");
+		} else {
+			movieTrailer(null, { tmdbId: movie?.id })
+				.then((url) => {
+					const urlParams = new URLSearchParams(new URL(url).search);
+					setTrailerUrl(urlParams.get("v"));
+				})
+				.catch((error) => console.log(error));
+		}
+	}
+	return (
+		<div className="row">
+			<h2>{title}</h2>
+			<div className="row__posters">
+				{movies.map((movie) => (
+					<img
+						key={movie.id}
+						onClick={() => handleClick(movie)}
+						className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+						src={`${baseurl}${isLargeRow ? movie.poster_path : movie.backdrop_path
+							}`}
+						alt={movie.name}
+					/>
+				))}
+			</div>
+			{trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+		</div>
+	);
 }
 
 export default Row;
