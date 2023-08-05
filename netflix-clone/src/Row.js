c7b0fb40cd09d6214c35cc4b4c69726237022ec0
vidhihihihihi <gupta.74@iitj.ac.in>
2023-08-05 17:18:10 +0530

    initial commit

---
+++ netflix-clone/src/Row.js
@@ -0,0 +1,37 @@
+import React , { useState, useEffect } from "react";
+import instance from "./axios";
+import "./Row.css";
+
+const baseurl = "http://image.tmdb.org/t/p/original/";
+
+function Row({title, fetchUrl}) {
+    const [movies, setMovies] = useState([]) ;
+    useEffect(() => {
+     async function fetchData() {
+        const request = await instance.get(fetchUrl); 
+        setMovies(request.data.results);
+        return request;
+     }
+     fetchData();
+    },[fetchUrl]);
+
+    console.log(movies);
+    return(
+        <div className='row'>
+            <h2>{title}</h2>
+            <div className='row_posters'>
+             {movies.map((movie)=>(
+                <img 
+                 key={movie.id}
+                 className="row_poster"
+                 src={`${baseurl}${movie.poster_path}`} 
+                 alt={movie.name}/>
+             ))}
+            </div>
+           
+
+        </div>
+    );
+}
+
+export default Row;
---
+++ netflix-clone/src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "dce4d42157aca47c6ea2f5845e483b87";
+
+const requests = {
+	fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+	fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+	fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+	fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+	fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+	fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+	fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+	fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
